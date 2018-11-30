60444977cac171ad018e3414a34a2703affdc6ae
David <david@Davids-Mac.local>
2018-11-30 07:17:15 -0800

    added menuscene

--- Feed Me/MenuScene.swift
+++ Feed Me/MenuScene.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2018 David Larkin. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+import AVFoundation
+
+class MenuScene: SKScene
+{
+    override func didMove(to view: SKView)
+    {
+        print("started menu")
+        setUpImage()
+        setUpButtons()
+    }
+    
+    fileprivate func setUpImage()
+    {
+        var background: SKSpriteNode!
+        background = SKSpriteNode(imageNamed: ImageName.Background)
+        background.anchorPoint = CGPoint(x: 0, y:0)
+        background.position = CGPoint(x: 0, y: 0)
+        background.size = self.size
+        background.zPosition = Layer.Background
+        addChild(background)
+        
+    }
+    
+    fileprivate func setUpButtons()
+    {
+        
+    }
+}
